48c05c58d59c8c1b218ea236439ed2200f43bf13
zekroTJA <zekrommaster110@aol.de>
2017-08-13 02:48:29 +0200

    created repo

---
+++ js/timer.js
@@ -0,0 +1,23 @@
+
+var GLOABL_DELAY = 5 * 60
+
+var containers = document.getElementsByClassName("container")
+var duration = parseInt(getComputedStyle(containers[0]).animationDuration.substring(0, 2))
+
+
+var delay = 0;
+for (var i = 0; i < 4; i++) {
+    containers[i].style.animationDelay = delay + "s"
+    containers[i].style.animationName = "fade"
+    delay += duration
+}
+
+function repeat() {
+    for (var i = 0; i < 4; i++) {
+        var e = containers[i]
+        e.parentNode.replaceChild(e.cloneNode(true), e)
+    }
+}
+
+
+setInterval(repeat, (GLOABL_DELAY + duration * containers.length) * 1000)
