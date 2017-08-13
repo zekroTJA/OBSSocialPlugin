9aefd3543a88cf4c7f5aa12c66c8be1a003768dd
zekroTJA <zekrommaster110@aol.de>
2017-08-13 17:44:11 +0200

    added comments and other stuff

--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,12 @@
+// Getting the animation delay out of the css root variables
+var GLOABL_DELAY = parseInt(getComputedStyle(document.querySelector("html")).getPropertyValue("--delay"))
 
-var animationDelay = parseInt(getComputedStyle(document.querySelector("html")).getPropertyValue("--delay"))
-
-
-var GLOABL_DELAY = animationDelay
-
+// Getting all divisions with the "containers" class
 var containers = document.getElementsByClassName("container")
+// Getting the duration of the animation out of the css file
 var duration = parseInt(getComputedStyle(containers[0]).animationDuration.substring(0, 2))
 
-
+// Sets the animation and delay to the container divisions
 var delay = 0;
 for (var i = 0; i < containers.length; i++) {
     containers[i].style.animationDelay = delay + "s"
@@ -15,6 +14,7 @@ for (var i = 0; i < containers.length; i++) {
     delay += duration
 }
 
+// Refreshes the container divisions that they will be animated again
 function repeat() {
     for (var i = 0; i < containers.length; i++) {
         var e = containers[i]
@@ -22,4 +22,5 @@ function repeat() {
     }
 }
 
+// Timer wich will repeat the animation
 setInterval(repeat, (GLOABL_DELAY + duration * containers.length) * 1000)
