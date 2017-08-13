4ac065d74e35a9bcaf56242f8c124c3316a205b2
zekroTJA <zekrommaster110@aol.de>
2017-08-13 02:52:19 +0200

    little js code update ^^

--- js/timer.js
+++ js/timer.js
@@ -6,14 +6,14 @@ var duration = parseInt(getComputedStyle(containers[0]).animationDuration.substr
 
 
 var delay = 0;
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < containers.length; i++) {
     containers[i].style.animationDelay = delay + "s"
     containers[i].style.animationName = "fade"
     delay += duration
 }
 
 function repeat() {
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < containers.length; i++) {
         var e = containers[i]
         e.parentNode.replaceChild(e.cloneNode(true), e)
     }
